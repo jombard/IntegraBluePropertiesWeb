1b81508448a1e4fab965eef295b166239dfd7a48
Jon Lombard <jombard@hotmail.com>
2020-06-27 21:47:58 +0100

    Init

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-700 text-white text-center py-5">
+    IntegraBlue Properties &copy; Copyright 2020
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="container">
+      <div>
+        <Logo />
+        <h1 class="title">
+          IntegraBlue Properties
+        </h1>
+        <p class="subtitle">We help solve property problems</p>
+        <div class="links">
+          <a href="#" class="button--green">
+            Contact Us
+          </a>
+          <a href="#" class="button--grey">
+            About
+          </a>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer'
+export default {
+  components: {
+    Footer,
+  },
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
