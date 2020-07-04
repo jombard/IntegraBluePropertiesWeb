efeb223824e171020ffd3754abb6a852a35d71fe
Jon Lombard <jombard@hotmail.com>
2020-07-04 15:35:06 +0100

    Move menu to component

---
+++ components/AppLinks.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li><nuxt-link to="/">Home</nuxt-link></li>
+    <li><nuxt-link to="/about">About</nuxt-link></li>
+    <li><nuxt-link to="/service">Service</nuxt-link></li>
+    <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+  </ul>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.sidenav {
+  li > a {
+    font-size: 1.5rem;
+    line-height: 3.5rem;
+  }
+}
+</style>
