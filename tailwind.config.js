24d41739cfcc0d25a0cc148c2a10e20f88db5d0a
Jon Lombard <jombard@hotmail.com>
2020-07-06 20:15:33 +0100

    Add theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    colors: {
+      blue: '#006298',
+      white: '#f1ede2',
+      sky: '#98c9e4',
+    },
+  },
   variants: {},
   plugins: [],
   purge: {
@@ -16,7 +22,7 @@ module.exports = {
       'layouts/**/*.vue',
       'pages/**/*.vue',
       'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
+      'nuxt.config.js',
+    ],
+  },
 }
