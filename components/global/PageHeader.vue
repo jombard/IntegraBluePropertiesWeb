1b4b0d0493e029d4c618f09ba0ee59bb6da7183f
Jon Lombard <jombard@hotmail.com>
2020-08-22 12:29:57 +0100

    Utilise utility classes

--- components/global/PageHeader.vue
+++ components/global/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-headline slide-in-from-left">
+  <div class="container mx-auto slide-in-from-left px-4 py-24 relative">
     <h1 class="font-bold mb-5">{{ title }}</h1>
     <div class="sub-header">
       <p>{{ subtitle }}</p>
@@ -24,39 +24,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.main-headline {
-  padding: 8em 4em 5em 3em;
+.sub-header {
+  margin-left: 20%;
   position: relative;
 
-  .sub-header {
-    margin-left: 20%;
-    position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    left: -100%;
+    right: 100%;
+    height: 100%;
+    background: #f1ede2;
+    margin-right: 3rem;
+  }
 
-    &:before {
-      content: '';
-      position: absolute;
-      left: -100%;
-      right: 100%;
-      height: 100%;
-      background: #f1ede2;
-      margin-right: 3rem;
-    }
-
-    p {
-      font-size: 1.2em;
-      line-height: 1.5em;
-    }
+  p {
+    font-size: 1.2em;
+    line-height: 1.5em;
   }
 }
 
 @media (min-width: 900px) {
-  .main-headline {
-    padding: 8em 4em 5em 5em;
-
-    .sub-header {
-      width: 40%;
-      margin-left: 15%;
-    }
+  .sub-header {
+    width: 40%;
+    margin-left: 15%;
   }
 }
 </style>
