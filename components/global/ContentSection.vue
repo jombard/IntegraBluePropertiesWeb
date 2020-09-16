92a97169164fc2781edc40b0992e6279cc09302d
Jon Lombard <jombard@hotmail.com>
2020-09-16 18:26:28 +0100

    Add component for consistent styling

---
+++ components/global/ContentSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto px-4 py-24 relative">
+    <div class="bg-white p-10">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentSection',
+}
+</script>
