51c3bd8e8139d79f19a30f8c48ae57f4f042d579
Jon Lombard <jombard@hotmail.com>
2020-09-19 17:08:05 +0100

    Add close menu action

--- store/menu.js
+++ store/menu.js
@@ -10,10 +10,16 @@ export const actions = {
   toggle({ commit }) {
     commit('TOGGLE')
   },
+  close({ commit }) {
+    commit('CLOSE')
+  },
 }
 
 export const mutations = {
   TOGGLE(state) {
     state.isOpen = !state.isOpen
+  },
+  CLOSE(state) {
+    state.isOpen = false
   },
 }
