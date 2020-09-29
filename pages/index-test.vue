96c80d8da3fbf1098b6fb643fb3fa332dad80ee8
Jon Lombard <jombard@hotmail.com>
2020-09-29 20:32:54 +0100

    Add services page

---
+++ pages/index-test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container mx-auto py-32 relative">
+    <div
+      style="background: blue; width: 300px; height: 400px"
+      class="absolute left-0 top-0"
+    ></div>
+    <div
+      style="background: #fff; border: 1px solid green"
+      class="w-1/2 mx-auto p-5 absolute"
+    >
+      <h2>
+        Headline call to action text that goes here and should be compelling to
+        read more
+      </h2>
+      <p>
+        Sub text that appears under the headline in smaller font and details the
+        upper text more
+      </p>
+      <button>Call to action</button>
+    </div>
+    <div
+      style="background: red; width: 250px; height: 250px"
+      class="absolute right-0 bottom-0"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/services.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <PageHeader
+      title="Services"
+      subtitle="How can we help you?"
+      class="text-blue"
+    />
+
+    <PrimarySection>
+      <div class="flex">
+        <div
+          v-for="(page, x) in pages"
+          :key="x"
+          class="w-1/4 text-white p-5 text-center"
+        >
+          <nuxt-link :to="page.path">
+            <div class="text-sm w-1/4 mx-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  :d="page.svg"
+                />
+              </svg>
+            </div>
+          </nuxt-link>
+          <h3 class="text-4xl">{{ page.title }}</h3>
+          <p>{{ page.description }}</p>
+          <nuxt-link
+            class="inline-flex items-center link-underline"
+            :to="page.path"
+            >Learn More
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </nuxt-link>
+        </div>
+      </div>
+    </PrimarySection>
+
+    <ContentSection>
+      <h3 class="text-4xl">Other services</h3>
+      <nuxt-link to="holiday-lets">Holiday Let Management</nuxt-link>
+    </ContentSection>
+
+    <SignUpForm title="Contact Us">
+      Please send us your details and we will get back to you
+    </SignUpForm>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const pages = await $content('').fetch()
+    return { pages }
+  },
+}
+</script>
