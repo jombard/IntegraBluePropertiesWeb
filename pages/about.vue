b48b08c29c58a1e3fe1c86e88423e35d6d8a84ef
Jon Lombard <jombard@hotmail.com>
2020-09-30 21:43:10 +0100

    Add text in blue

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <PageHeader
-      title="About Us"
-      subtitle="More about IntegraBlue Properties"
-      class="text-blue"
-    />
+    <PageHeader title="About Us" class="text-blue" />
 
-    <PrimarySection :headline="page.title" :alt-text="page.description" />
+    <PrimarySection
+      :headline="page.title"
+      :alt-text="page.description"
+      image-url="talk.jpg"
+    />
 
     <ContentSection>
       <nuxt-content :document="page" />
     </ContentSection>
 
-    <div class="container mx-auto w-1/4 pb-10">
-      <img src="~/assets/images/talk.jpg" alt="Jon Lombard giving a talk" />
-    </div>
+    <PrimarySection headline="Who We Are">
+      <div class="text-white">
+        <p>
+          Jon Lombard started IntegraBlue Properties to bring an open and honest
+          business to the property market. He is primarily interested in working
+          with HMO landlords and holiday home owners.
+        </p>
+        <p>
+          Jon lives in Dorset with his wife and daughter, and when he is not in
+          front of the computer working on his web business or spending time
+          with his family, he can be found running around the trails of the
+          hilly Dorset coastline.
+        </p>
+      </div>
+    </PrimarySection>
   </div>
 </template>
 
