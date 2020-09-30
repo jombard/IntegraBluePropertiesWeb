e2a66553d8763b419b4e0befea789cb5eb54292a
Jon Lombard <jombard@hotmail.com>
2020-09-30 21:44:30 +0100

    Add image

--- pages/holiday-lets.vue
+++ pages/holiday-lets.vue
@@ -2,13 +2,13 @@
   <div>
     <PageHeader
       title="Holiday Lets"
-      subtitle="Are you looking for holiday let management? We may be able to help!"
+      subtitle="Are you looking for holiday let management?"
       class="text-blue"
     />
 
     <PrimarySection
       headline="Offering solutions for holiday home owners"
-      alt-text="Offering solutions for holiday home owners"
+      image-url="holidayhome.jpg"
     />
 
     <ContentSection>
