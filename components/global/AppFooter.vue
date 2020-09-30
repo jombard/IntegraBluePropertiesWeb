3be4087261dd30d37ba11b4dc35eb737362bd7e6
Jon Lombard <jombard@hotmail.com>
2020-09-30 21:39:16 +0100

    Add pin to footer

--- components/global/AppFooter.vue
+++ components/global/AppFooter.vue
@@ -1,15 +1,21 @@
 <template>
-  <footer class="fixed bottom-0 w-full bg-gray-900 py-12 text-gray-500">
+  <footer class="fixed bottom-0 w-full bg-gray-900 py-10 text-gray-500">
     <div class="container mx-auto px-4">
       <div class="flex flex-col md:flex-row">
-        <div class="mb-5 md:w-1/3">
+        <div class="mb-5 md:mb-0 md:w-1/3">
           <strong>IntegraBlue Properties</strong><br />
           Registered Company in England no: 12247772<br />
           &copy; Copyright 2020
         </div>
-        <div class="mb-5 md:w-1/3"></div>
-        <div class="mb-5 md:w-1/3">
+        <div class="mb-5 md:mb-0 md:w-1/3"></div>
+        <div class="mb-5 md:mb-0 md:w-1/3">
           <strong>Registered with</strong><br />
+          <a
+            href="https://propertyinvestorsnetwork.co.uk/"
+            target="_blank"
+            rel="nofollow norefer"
+            >Property Investors Network</a
+          ><br />
           <a href="https://ico.org.uk/" target="_blank" rel="nofollow norefer"
             >ICO</a
           ><br />
