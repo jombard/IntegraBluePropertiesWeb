9bc12c9464178191e31dbbbaf87732392256d44e
Jon Lombard <jombard@hotmail.com>
2021-01-07 07:13:47 +0000

    Remove other service and sort pages

--- pages/services.vue
+++ pages/services.vue
@@ -17,7 +17,7 @@
         <div
           v-for="(page, x) in pages"
           :key="x"
-          class="md:w-1/4 p-5 text-center"
+          class="md:w-1/3 p-5 text-center mx-auto"
         >
           <nuxt-link :to="page.path" class="text-blue">
             <div class="text-sm w-1/4 mx-auto">
@@ -58,47 +58,6 @@
           </nuxt-link>
         </div>
       </div>
-
-      <div class="pt-10 text-center md:w-1/4 mx-auto">
-        <h2 class="text-2xl mb-10">Other services</h2>
-        <nuxt-link to="holiday-lets" class="text-blue">
-          <div class="text-sm w-1/4 mx-auto">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </div>
-        </nuxt-link>
-        <h2 class="text-3xl mb-2 font-bold">
-          <nuxt-link to="/holiday-lets">Holiday Let Management</nuxt-link>
-        </h2>
-        <p>Offering solutions for holiday home owners</p>
-        <nuxt-link
-          class="inline-flex items-center link-underline text-blue"
-          to="holiday-lets"
-          >Learn More
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-4 h-4 ml-2"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </nuxt-link>
-      </div>
     </ContentSection>
 
     <SignUpForm title="Contact Us">
@@ -111,7 +70,7 @@
 export default {
   async asyncData({ $content }) {
     const pages = await $content('').fetch()
-    return { pages }
+    return { pages: pages.sort((a, b) => a.order - b.order) }
   },
 }
 </script>
