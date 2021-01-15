ce154c9cb6bb14d5c297f3b2e15dfdd487f31b8c
Jon Lombard <jombard@hotmail.com>
2021-01-15 22:48:23 +0000

    Remove margin

--- components/global/TopPageLinks.vue
+++ components/global/TopPageLinks.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="container mx-auto flex flex-row py-2">
-    <a
-      href="mailto:hello@integrablue.co.uk"
-      class="mr-5 w-10 md:w-6 mb-5 md:mb-0"
-    >
+    <a href="mailto:hello@integrablue.co.uk" class="mr-5 w-10 md:w-6 mb-0">
       <EmailIcon />
       <span class="sr-only">Email</span>
     </a>
-    <a href="tel:01308 800 291" class="mr-5 w-10 md:w-6 mb-5 md:mb-0">
+    <a href="tel:01308 800 291" class="mr-5 w-10 md:w-6 mb-0">
       <PhoneIcon />
       <span class="sr-only">Phone</span>
     </a>
     <a
       href="https://twitter.com/integrablueprop"
       target="_blank"
-      class="mr-5 w-10 md:w-6 mb-5 md:mb-0"
+      class="mr-5 w-10 md:w-6 mb-0"
       rel="nofollow norefer"
     >
       <TwitterIcon />
@@ -23,7 +20,7 @@
     <a
       href="https://www.facebook.com/integrablueproperties/"
       target="_blank"
-      class="mr-5 w-10 md:w-6 mb-5 md:mb-0"
+      class="mr-5 w-10 md:w-6 mb-0"
       rel="nofollow norefer"
     >
       <FacebookIcon />
@@ -32,7 +29,7 @@
     <a
       href="https://www.instagram.com/integrablueproperties/"
       target="_blank"
-      class="mr-5 w-10 md:w-6 mb-5 md:mb-0"
+      class="mr-5 w-10 md:w-6 mb-0"
       rel="nofollow norefer"
     >
       <InstagramIcon />
